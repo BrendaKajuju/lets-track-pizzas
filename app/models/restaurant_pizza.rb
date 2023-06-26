1cdf19f58c0c41220a34df1c7a9e97ef5767bbe4
Brenda <brendakajuju777@gmail.com>
2023-06-26 23:39:31 +0300

    add validations for the price

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,6 @@
 class RestaurantPizza < ApplicationRecord
   belongs_to :restaurant
   belongs_to :pizza
+# validate that the price exists within the given range
+  validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 30}
 end
